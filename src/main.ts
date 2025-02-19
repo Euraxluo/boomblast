65a47cde3b638a2afae42aba3e4203f172f446ba
Euraxluo <euraxluo@outlook.com>
2025-02-19 15:56:59 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import {NestFactory} from "@nestjs/core";
 import {ValidationPipe} from "@nestjs/common";
 import {Redis} from "ioredis";
+import {NestExpressApplication} from "@nestjs/platform-express";
+import {Request, Response} from "express";
 
 import {AppModule} from "./modules/app";
 import {clusterize} from "./lib/cluster";
@@ -8,8 +10,10 @@ import {WebSocketAdapter} from "./lib/ws";
 import {REDIS_PROVIDER_TOKEN} from "./lib/redis";
 import {session} from "./lib/session";
 
+let app: NestExpressApplication;
+
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule, {
+  app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       credentials: true,
       origin: process.env.CLIENT_ORIGIN,
@@ -22,9 +26,23 @@ async function bootstrap(): Promise<void> {
   app.useWebSocketAdapter(new WebSocketAdapter(app, true));
   app.useGlobalPipes(new ValidationPipe());
 
-  const port = process.env.PORT || 8000;
-  await app.listen(port);
-  console.log(`Application is running on port ${port}`);
+  if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT || 8000;
+    await app.listen(port);
+    console.log(`Application is running on port ${port}`);
+  }
 }
 
-clusterize(bootstrap);
+if (process.env.NODE_ENV !== 'production') {
+  clusterize(bootstrap);
+} else {
+  bootstrap();
+}
+
+// 为 Vercel 导出 Express 实例
+export default async function handler(req: Request, res: Response): Promise<void> {
+  if (!app) {
+    await bootstrap();
+  }
+  return app.getHttpAdapter().getInstance()(req, res);
+};
