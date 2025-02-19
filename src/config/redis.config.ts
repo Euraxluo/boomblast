bb82b858c3d6dd999b024fd5cf10b9bf6c041336
Euraxluo <euraxluo@outlook.com>
2025-02-19 18:20:16 +0800

    fix redis

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -8,8 +8,12 @@ export const redisConfig = registerAs("redis", () => {
     port: parseInt(env.REDIS_PORT, 10),
     password: env.REDIS_PASSWORD,
     username: "default",
-    tls: {
-      rejectUnauthorized: false
-    },
+    tls: true,
+    retryStrategy: (times: number): number | null => {
+      // 最多重试 5 次
+      if (times > 5) return null;
+      // 重试间隔时间，单位毫秒
+      return Math.min(times * 1000, 3000);
+    }
   };
 });
